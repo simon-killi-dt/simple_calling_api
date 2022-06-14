99f988585912d8bad55581cb68fb4c977c5c1392
simon-killi-dt <73580674+simon-killi-dt@users.noreply.github.com>
2022-06-14 20:31:02 +0200

    final 1.1

--- src/CardProduct.jsx
+++ src/CardProduct.jsx
@@ -7,13 +7,13 @@ export default class CardProduct extends Component {
         <div className="card-body">
           <h5 className="card-name">{person.name}</h5>
           <p className="card-email">{person.email}</p>
-          <button className="card-conpamy.name" onClick={() => myFunc(person)}>
+          <button className="card-conpamy.name" onClick={() => myFunc()}>
             myButton
           </button>
         </div>
       </div>
     );
-    function myFunc(person) {
+    function myFunc() {
       alert(person.company.name);
     }
   }
