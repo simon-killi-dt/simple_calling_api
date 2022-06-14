071ffc3f6bdfc30a609a3aa3d6fe3330722ff96c
simon-killi-dt <73580674+simon-killi-dt@users.noreply.github.com>
2022-06-14 20:20:50 +0200

    final

--- src/CardProduct.jsx
+++ src/CardProduct.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 export default class CardProduct extends Component {
   render() {
+    var person = this.props.data;
     return (
       <div className="card">
         <div className="card-body">
-          <h5 className="card-name">
-            {this.props.data.name}, {this.props.data.id}
-          </h5>
-          <p className="card-email">{this.props.data.email}</p>
-          <button onClick>Show More</button>
+          <h5 className="card-name">{person.name}</h5>
+          <p className="card-email">{person.email}</p>
+          <button className="card-conpamy.name" onClick={() => myFunc(person)}>
+            myButton
+          </button>
         </div>
       </div>
     );
+    function myFunc(person) {
+      alert(person.company.name);
+    }
   }
 }
